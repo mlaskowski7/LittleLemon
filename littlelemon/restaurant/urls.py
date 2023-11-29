06eb314624100b1503f237255072a663627833a2
mlaskowski7 <mtlaskowski7@gmail.com>
2023-11-29 23:44:39 +0100

    creating api views using django rest framework

--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -1,6 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
+
+
 urlpatterns = [
-    path('',views.index, name='index')
+    path('menu/',views.MenuItemView.as_view()),
+    path('menu/<int:pk>',views.SingleMenuItemView.as_view()),
+    path('home/',views.home)
+   
 ]
