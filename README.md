53e19d3099712c3235846dc2b4619855b35a3925
mlaskowski7 <mtlaskowski7@gmail.com>
2023-11-30 11:57:36 +0100

    final commit

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This is a sample Little Lemon restaurant website.
 Back End created in Python (Django, Django REST Framework) integrated with MySQL database.
 Front End created in HTML, CSS, JAVASCRIPT.
+apis endpoints : /restaurant/api/menu/, /restaurant/api/booking/, /auth, /restaurant/api-token-auth/, restaurant/api/menu/<int:pk>
--- littlelemon/restaurant/urls.py
+++ littlelemon/restaurant/urls.py
@@ -5,9 +5,9 @@ from rest_framework.authtoken.views import obtain_auth_token
 
 
 urlpatterns = [
-    path('menu/',views.MenuItemView.as_view()),
-    path('menu/<int:pk>',views.SingleMenuItemView.as_view()),
-    path('home/',views.home),
+    path('api/menu/',views.MenuItemView.as_view()),
+    path('api/menu/<int:pk>',views.SingleMenuItemView.as_view()),
+    path('',views.home),
     path('api-token-auth/',obtain_auth_token)
    
 ]
